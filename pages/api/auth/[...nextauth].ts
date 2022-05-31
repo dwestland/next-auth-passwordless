4d647392591d954e5d5b2d55a532a7f71b7a0738
dwestland <don@westland.net>
2022-05-31 07:57:31 -0700

    Update Prisma schema

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,25 @@
 import NextAuth from 'next-auth'
-// import { NextApiHandler } from 'next'
-
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
-
 import GithubProvider from 'next-auth/providers/github'
-
 import GoogleProvider from 'next-auth/providers/google'
-
-// import EmailProvider from 'next-auth/providers/email'
+import EmailProvider from 'next-auth/providers/email'
 
 const prisma = new PrismaClient()
 
+// // too many connections during local development
+// let prisma: PrismaClient
+
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient()
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient()
+//   }
+//   prisma = global.prisma
+// }
+
 export default NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -24,20 +30,24 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // EmailProvider({
-    //   server: {
-    //     host: process.env.EMAIL_SERVER_HOST,
-    //     port: process.env.EMAIL_SERVER_PORT,
-    //     auth: {
-    //       user: process.env.EMAIL_SERVER_USER,
-    //       pass: process.env.EMAIL_SERVER_PASSWORD,
-    //     },
-    //     from: process.env.EMAIL_FROM,
-    //   },
-    // }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+        from: process.env.EMAIL_FROM,
+      },
+    }),
   ],
-
+  adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
+  theme: {
+    colorScheme: 'light',
+    logo: '/vercel.svg',
+  },
 })
 
 // const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
